322e6ddb4684701379c7bf1605319f28b4e1803f
colinrjrobbins <colinrjrobbins@gmail.com>
2019-12-01 11:31:55 -0500

    Adjusted if statements

--- buttonTest.py
+++ buttonTest.py
@@ -8,9 +8,9 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    if GPIO.input(10) == True:
+    if (GPIO.input(10)):
         print("Emergency Stop")
-    elif GPIO.input(11) == True:
+    elif (GPIO.input(11)):
         print("Reset")
     else:
         waiting = input("Waiting...")
