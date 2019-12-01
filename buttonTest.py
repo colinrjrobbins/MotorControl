b9dcad9c61f7402e0a6b31b568f6448906c1b504
colinrjrobbins <colinrjrobbins@gmail.com>
2019-12-01 11:27:57 -0500

    added a button test

--- buttonTest.py
+++ buttonTest.py
@@ -7,3 +7,10 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+while True:
+    if GPIO.input(10) == True:
+        print("Emergency Stop")
+    elif GPIO.input(11) == True:
+        print("Reset")
+    else:
+        waiting = input("Waiting...")
